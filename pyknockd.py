3b377b7f9eda839e5443efbdb0a168c3ab2fff36
Vladislav Yarmak <vladislav@vm-0.com>
2017-08-13 02:50:32 +0300

    use argparse for server options

--- pyknockd.py
+++ pyknockd.py
@@ -6,26 +6,21 @@ import os
 import hashlib
 import hmac
 import string
-
-def usage():
-    print >> sys.stderr, (
-            "Usage: %(progname)s <listen port> <PSK> <open command template> <close command template>\n"
-            "Example:\n"
-            "%(progname)s 60120 MySecretPSK 'ipset add myallowedset $ip timeout 3600' 'ipset del myallowedset $ip'"
-            ) % dict(progname = sys.argv[0])
-    sys.exit(2)
+import argparse
 
 def main():
-    if len(sys.argv) != 5:
-        usage()
-
-    port, psk, open_command_tpl, close_command_tpl = sys.argv[1:5]
-    port = int(port)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bind-address", help="bind address", default="")
+    parser.add_argument("-p", "--port", help="bind port", type=int, default=60120, metavar="PORT")
+    parser.add_argument("psk", help="pre-shared key used to authenticate clients", metavar="PSK")
+    parser.add_argument("open_cmd", help="template of command used to enable access. Example: \"ipset add -exist myset $ip\". Available variables: $ip, $port", metavar="OPEN_CMD")
+    parser.add_argument("close_cmd", help="template of command used to disable access. Example: \"ipset del -exist myset $ip\". Available variables: $ip, $port", metavar="CLOSE_CMD")
+    args = parser.parse_args()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(("", port))
-    open_cmd = string.Template(open_command_tpl)
-    close_cmd = string.Template(close_command_tpl)
+    s.bind((args.bind_address, args.port))
+    open_cmd = string.Template(args.open_cmd)
+    close_cmd = string.Template(args.close_cmd)
     try:
         while True:
             data, addr = s.recvfrom(4096)
@@ -37,7 +32,7 @@ def main():
             signature = data[1:]
             if hmac.compare_digest(
                     hmac.new(
-                            psk, opcode + ip4, hashlib.sha256
+                            args.psk, opcode + ip4, hashlib.sha256
                         ).digest(),
                     signature):
                 if opcode == '\x01':
