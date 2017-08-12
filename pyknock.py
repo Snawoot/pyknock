8174c35884fb76cd721edf5aa8c438b3cf872560
Vladislav Yarmak <vladislav@vm-0.com>
2017-08-13 00:58:48 +0300

    add sign address option; tidy

--- pyknock.py
+++ pyknock.py
@@ -6,22 +6,22 @@ import hashlib
 import hmac
 import argparse
 
-#def usage():
-#    print >> sys.stderr, "Usage: %s <open|close> <address> <port> <psk>" % sys.argv[0]
-#    sys.exit(2)
-#
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("command", help="command: open or close", choices=["open", "close"])
     parser.add_argument("address", help="remote address")
     parser.add_argument("port", help="remote port", type=int)
     parser.add_argument("psk", help="pre-shared key used to authenticate ourselves to knocked peer")
+    parser.add_argument("-s", "--sign-address", help="sign specified address instead of socket source address")
     args = parser.parse_args()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((args.address, args.port))
 
-    myip = socket.inet_pton(socket.AF_INET, s.getsockname()[0])
+    if args.sign_address:
+        myip = socket.inet_pton(socket.AF_INET, args.sign_address)
+    else:
+        myip = socket.inet_pton(socket.AF_INET, s.getsockname()[0])
     opcode = '\x01' if args.command == 'open' else '\x02'
     digest = hmac.new(args.psk, opcode + myip, hashlib.sha256).digest()
     s.sendall(opcode + digest)
