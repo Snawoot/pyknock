d683c3c114023468f6139161305190d278d2486a
Vladislav Yarmak <vladislav@vm-0.com>
2017-10-24 15:06:44 +0300

    proper address family handling

--- pyknock.py
+++ pyknock.py
@@ -14,22 +14,12 @@ CODE_CLOSE = 2
 
 
 def detect_af(addr):
-    A = None
-    try:
-        A = socket.inet_pton(socket.AF_INET, addr)
-    except socket.error:
-        pass
-    else:
-        return (socket.AF_INET, A)
-
-    try:
-        A = socket.inet_pton(socket.AF_INET6, addr)
-    except socket.error:
-        pass
-    else:
-        return (socket.AF_INET6, A)
-
-    return (None, A)
+    return socket.getaddrinfo(addr,
+                              None,
+                              socket.AF_UNSPEC,
+                              0,
+                              0,
+                              socket.AI_NUMERICHOST)[0][0]
 
 
 def parse_args():
@@ -62,34 +52,46 @@ def parse_args():
 def main():
     args = parse_args()
 
-    dst_ai = socket.getaddrinfo(args.address,
-                                args.port,
-                                socket.AF_UNSPEC,
-                                socket.SOCK_DGRAM,
-                                socket.IPPROTO_UDP)
-    if not dst_ai:
-        print >> sys.stderr, "Destination address resolution error."
-        sys.exit(3)
-
-    if args.source_address:
-        src_ai = socket.getaddrinfo(args.address,
-                                    None,
+    try:
+        dst_ai = socket.getaddrinfo(args.address,
+                                    args.port,
                                     socket.AF_UNSPEC,
                                     socket.SOCK_DGRAM,
                                     socket.IPPROTO_UDP)
-        if not src_ai:
-            print >> sys.stderr, "Source address resolution error."
-            sys.exit(3)
+    except Exception as e:
+        print >> sys.stderr, ("Unable to resolve destination address %s: %s" %
+                              (repr(args.address), str(e)))
+        sys.exit(3)
+    assert dst_ai, "Destionation address info must not be empty"
+
+    src_af = socket.AF_UNSPEC
+    if args.source_address:
+        try:
+            src_af = detect_af(args.source_address)
+        except:
+            print >> sys.stderr, "Malformed source address"
+            sys.exit(4)
+
+    sign_af = socket.AF_UNSPEC
+    if args.sign_address:
+        try:
+            sign_af = detect_af(args.sign_address)
+        except:
+            print >> sys.stderr, "Malformed sign address"
+            sys.exit(5)
 
     for ai_entry in dst_ai:
         af = ai_entry[0]
+        if src_af != socket.AF_UNSPEC and af != src_af:
+            continue
+
         s = socket.socket(af, socket.SOCK_DGRAM)
         if args.source_address:
             s.bind((args.source_address, 0))
         s.connect((args.address, args.port))
 
         if args.sign_address:
-            myip = socket.inet_pton(af, args.sign_address)
+            myip = socket.inet_pton(sign_af, args.sign_address)
         else:
             myip = socket.inet_pton(af, s.getsockname()[0])
 
